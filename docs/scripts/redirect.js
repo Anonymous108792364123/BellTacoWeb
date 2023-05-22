25b6e82c7dd67fc718acaaa4c88f4379c1599b98
Error404 <129235236+Error404-1@users.noreply.github.com>
2023-05-22 13:46:40 -0500

    Update redirect.js

--- docs/scripts/redirect.js
+++ docs/scripts/redirect.js
@@ -1,6 +1,9 @@
-document.getElementById('button').onclick = function() {
+document.getElementById('sure').onclick = function() {
   var url_string = window.location;
   var url = new URL(url_string);
   var target = url.searchParams.get("url");
   window.location.href = "url";
+};
+document.getElementById('back').onclick = function() {
+  history.back()
 };
